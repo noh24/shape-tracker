27a22f03247fdb88965d9c5a8625bad239496c29
Brian Noh <noh24@ymail.com>
2022-11-07 22:06:12 -0500

    Clear iso triangle test

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -15,4 +15,8 @@ describe('Triangle', () => {
     const scalTriangle = new Triangle(4,5,7);
     expect(scalTriangle.checkType()).toEqual("scalene triangle");
   });
+  test ('should correctly determine whether three lengths make a isosceles triangle', () => {
+    const isoTriangle = new Triangle(5, 5, 7);
+    expect(isoTriangle.checkType()).toEqual("iso triangle");
+  });
 });
